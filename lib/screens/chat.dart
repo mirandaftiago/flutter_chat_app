239224445a79d045b544f227553dd46e221dabe1
Tiago Miranda <miranda.f.tiago@gmail.com>
2023-10-18 12:44:00 +0100

    push notifications logic based on FirebaseMessaging

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:chat_app/widgets/chat_messages.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    // request permission to receive notifications
+    await fcm.requestPermission();
+
+    final token = fcm.getToken();
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
